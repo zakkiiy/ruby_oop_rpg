9ebd8dc2be864711c3d054b50ab662899e752a63
zakkiiy <131367248+zakkiiy@users.noreply.github.com>
2023-08-05 12:53:40 +0900

    chapter2 ok

--- main.rb
+++ main.rb
@@ -1,15 +1,4 @@
 # 勇者クラス
-# クラスは実体化されたインスタンスを使って利用されます。
-# クラスのインスタンスを生成することで、そのクラスの機能や属性を利用できる
-
-# セッターとは、インスタンス変数の値をクラス内で、更新するためのメソッドです。
-# そのセッターで引数で受け取った値をインスタンス変数に代入する。
-# セッターでパラメータを取得する
-
-# ゲッターで取り出せる。ゲッターとはクラス内にあるインスタンス変数を取得し、クラス外から参照できるようにするためのメソッド。
-
-# initializeメソッドを使用することで、インスタンス化されると同時に、クラスのインスタンス変数に初期値を設定できる。
-
 class Brave
   # attr_readerの記述でゲッターを省略できる
   attr_reader :name, :hp, :offense, :defense
@@ -17,74 +6,15 @@ class Brave
   # offenceやdefenseは値を変えてはダメなので、セッターをつけない。
   attr_accessor :hp
 
-
-
-  # initializeメソッド new演算子から渡された引数を受け取る。
-  # キーワード引数を使用する
-  # def initialize(name:, hp:, offense:, defense:)
-  # 各パラメータをハッシュで取得。 インスタンス生成時にハッシュで値を渡すことができる。
-  # **をつけることで、ハッシュ形式のみ受け付ける。
   def initialize(**params)
     @name = params[:name]
     @hp = params[:hp]
     @offense = params[:offense]
     @defense = params[:defense]
   end
-
-  # nameのセッター
-  # def name=(name)
-  #   @name = name
-  # end
-
-  # nameのゲッター
-  # def name
-  #   @name
-  # end
-
-  # hpのセッターを再定義
-  # def hp=(hp)
-  #   @hp = hp
-  # end
-
-  # hpのゲッター
-  # def hp
-  #   @hp
-  # end
-
-  # offenseのセッター
-  # def offense=(offense)
-  #   @offense = offense
-  # end
-
-  # offenseのゲッター
-  # def offense
-  #   @offense
-  # end
-
-  # defenseのセッター
-  # def defense=(defense)
-  #   @defense = defense
-  # end
-
-  # defenseのゲッター
-  # def defense
-  #   @defense
-  # end
-
 end
 
-# Braveクラスの新しいインスタンスを作成し、braveはそのクラスのオブジェクト（インスタンス）という。
-# このプロセスをインスタンス化という。これにより、Braveクラスで定義されたメソッドや属性を使用できる。
-# brave = "テリー"は、braveオブジェクトのname属性に値を設定している。
-# nameはbraveクラスのインスタンス変数
-# 特定のbraveインスタンスのname属性に値てテリーを代入している。
-
 brave = Brave.new(name: "テリー", hp: 500, offense: 150, defense: 100)
-
-# brave.name = "テリー"
-# brave.hp = 500
-# brave.offense = 150
-# brave.defense = 100
 
 # ゲッターで取り出す。
 puts "NAME：#{brave.name}"
@@ -94,3 +24,23 @@ puts "DEFENSE：#{brave.defense}"
 
 brave.hp -= 30
 puts brave.hp
+
+# モンスタークラス
+class Monster
+  attr_reader :name, :offense, :defense
+  attr_accessor :hp
+
+  def initialize(**params)
+    @name = params[:name]
+    @hp = params[:hp]
+    @offense = params[:offense]
+    @defense = params[:defense]
+  end
+end
+
+monster = Monster.new(name: "スライム", hp: 250, offense: 200, defense: 100)
+
+puts "NAME：#{monster.name}"
+puts "HP：#{monster.hp}"
+puts "OFFENSE：#{monster.offense}"
+puts "DEFENSE：#{monster.defense}"
